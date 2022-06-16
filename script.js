5e48c86a854cf2310110fca6f1dd06c7fbdff446
Timm Guethle <timm.guethle@gmail.com>
2022-06-16 19:18:45 +0200

    display data

--- script.js
+++ script.js
@@ -10,11 +10,19 @@ function stop_sound() {
 }
 
 function handleEvents(event) {
+	var e_x = document.getElementById('value_x')
+	var e_y = document.getElementById('value_y')
+	var e_z = document.getElementById('value_z')
+
 	var x = event.acceleration.x
 	var y = event.acceleration.y
 	var z = event.acceleration.z
 
-	console.log(x, y, z)
+	e_x.textContent = x
+	e_y.textContent = y
+	e_z.textContent = z
+
+	
 
 }
 
