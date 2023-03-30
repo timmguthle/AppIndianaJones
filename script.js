44be2db99e5759b33592d6bf79c6fc5923fef686
Timm Guethle <timm.guethle@gmail.com>
2023-03-30 13:35:09 +0200

    display motion

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function handleEvents(event) {
 	var y = event.acceleration.y
 	var z = event.acceleration.z
 
-	e_x.textContent = x.toString()
-	e_y.textContent = y.toString()
-	e_z.textContent = z.toString()
+	e_x.innerHTML = x.toString()
+	e_y.innerHTML = y.toString()
+	e_z.innerHTML = z.toString()
 
 	
 
